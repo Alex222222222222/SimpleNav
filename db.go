b7b0b4aa858b9d32a13b75f4c958df3d2015b29e
Alex Hua <huazifan@gmail.com>
2022-10-11 17:25:35 +0800

    finish the function to add links
    finish the real function to add a link, the function to fetch
    information from url does not finished yet

--- db.go
+++ db.go
@@ -4,6 +4,9 @@ import (
 	"database/sql"
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
+	"os"
 	"strconv"
 	"strings"
 
@@ -12,6 +15,18 @@ import (
 
 var db *sql.DB
 
+/*
+CREATE TABLE IF NOT EXISTS Links (
+	ID INT PRIMARY KEY,
+	Name TEXT,
+	URL TEXT,
+	Description TEXT,
+	IMG TEXT,
+	FatherCategory TEXT,
+	Priority int,
+	Tags TEXT
+);
+*/
 type Link struct {
 	ID             int      // the unique id of a single link
 	Name           string   // the name of the link
@@ -23,6 +38,18 @@ type Link struct {
 	Tags           []string // tags of the links
 }
 
+/*
+CREATE TABLE IF NOT EXISTS Category (
+	ID INT PRIMARY KEY,
+	Name TEXT,
+	FatherCategory TEXT,
+	SubCategory TEXT,
+	Links TEXT,
+	Description TEXT,
+	Hidden BOOLEAN,
+	Priority int
+);
+*/
 type Category struct {
 	ID             int    // unique id of a category
 	Name           string // name of the category
@@ -34,6 +61,25 @@ type Category struct {
 	Priority       int    // priority used in the ordering of links
 }
 
+/*
+CREATE TABLE IF NOT EXISTS Tags (
+	ID INT PRIMARY KEY,
+	Name TEXT,
+	Links TEXT,
+	Description TEXT,
+	Hidden BOOLEAN,
+	Priority int
+);
+*/
+type Tag struct {
+	ID          int    // unique id of a tag
+	Name        string // name of the tag
+	Links       []int  // all links
+	Description string // description
+	Hidden      bool   // hidden without the cookies?
+	Priority    int    // priority used in the ordering of tags
+}
+
 // store all links
 var Links map[int]*Link
 
@@ -43,6 +89,11 @@ var Categories []int
 // store all category
 var AllCategories map[int]*Category
 
+// TODO load all tags at the LoadData() func
+// store a tags
+var Tags map[int]*Link
+
+// TODO have bug in json.UnMarshal, maybe caused by empty data in the database, need to deal with the empty data
 // load all category and links to ram for render purpose
 func LoadData() (err error) {
 
@@ -177,6 +228,20 @@ func DBInit() (err error) {
 	}
 
 	_, err = db.Exec(
+		`CREATE TABLE IF NOT EXISTS Tags (
+			ID INT PRIMARY KEY,
+			Name TEXT,
+			Links TEXT,
+			Description TEXT,
+			Hidden BOOLEAN,
+			Priority int
+		);`,
+	)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(
 		`CREATE TABLE IF NOT EXISTS Category (
 			ID INT PRIMARY KEY,
 			Name TEXT,
@@ -211,7 +276,7 @@ func DBInit() (err error) {
 	return nil
 }
 
-// TODO add category
+// add category
 // insert a new category to database
 // also insert itself to the list of subcategories of its father category
 //
@@ -270,26 +335,28 @@ func AddCategory(name string, fc []string, d string, hidden bool, priority int) 
 
 		} else if err != nil {
 			return err
-		}
-
-		if len(sbs) == 0 {
-			sb = make([]int64, 0, 1)
 		} else {
-			err = json.Unmarshal(sbs, &sb)
+
+			if len(sbs) == 0 {
+				sb = make([]int64, 0, 1)
+			} else {
+				err = json.Unmarshal(sbs, &sb)
+				if err != nil {
+					return err
+				}
+			}
+
+			sb = append(sb, int64(id))
+			sbs, err = json.Marshal(sb)
 			if err != nil {
 				return err
 			}
-		}
 
-		sb = append(sb, int64(id))
-		sbs, err = json.Marshal(sb)
-		if err != nil {
-			return err
-		}
+			_, err = db.Exec("UPDATE Category SET SubCategory = ? WHERE ID = ?", sbs, idj)
+			if err != nil {
+				return err
+			}
 
-		_, err = db.Exec("UPDATE Category SET SubCategory = ? WHERE ID = ?", sbs, idj)
-		if err != nil {
-			return err
 		}
 
 	}
@@ -297,7 +364,8 @@ func AddCategory(name string, fc []string, d string, hidden bool, priority int) 
 	return nil
 }
 
-// TODO add links
+// TODO need to be test
+// add links
 // insert a new link to database and copy the img file to static/img/front/id.png
 // also, automatically run the FetchLinkInformation func to fill missing information
 // also insert itself to the list of links of its father category
@@ -305,6 +373,147 @@ func AddCategory(name string, fc []string, d string, hidden bool, priority int) 
 // d means description
 // img represent the given path of the img, if the img does not exist or the target path is not a img, then return a error
 func AddLink(name string, url string, d string, img string, fc []string, priority int, tags []string) (err error) {
+
+	// get the id
+	var id int
+	err = db.QueryRow("SELECT ID FROM Links ORDER BY ID DESC").Scan(&id)
+	if errors.Is(err, sql.ErrNoRows) {
+		id = 1
+	} else if err != nil {
+		return err
+	}
+	id += 1
+
+	// parse the father categories
+	fcl := make([]int64, 0, len(fc))
+	var fci int64
+	var fct []string
+	for i := 0; i < len(fc); i += 1 {
+		fct = strings.Split(fc[i], ":")
+		for j := 0; j < len(fct); j += 1 {
+			if len(fct[j]) > 0 {
+				fci, err = strconv.ParseInt(fct[j], 10, 64)
+				fcl = append(fcl, fci)
+			}
+		}
+	}
+	if len(fcl) == 0 {
+		return errors.New("at least on father category must be provided for a link")
+	}
+	fcs, err := json.Marshal(fcl)
+
+	// parse the tags
+	tl := make([]int64, 0, len(tags))
+	var tt []string
+	var ti int64
+	for i := 0; i < len(tags); i += 1 {
+		tt = strings.Split(tags[i], ":")
+		for j := 0; j < len(tt); j += 1 {
+			if len(tt[j]) > 0 {
+				ti, err = strconv.ParseInt(tt[j], 10, 64)
+				tl = append(tl, ti)
+			}
+		}
+	}
+	ts, err := json.Marshal(tl)
+
+	// fetch and fill the missing information
+	ir := false
+	if img == "" {
+		ir = true
+	}
+	l, err := FetchLinkInformation(url, id, ir)
+	if name == "" {
+		name = l.Name
+	}
+	if d == "" {
+		d = l.Description
+	}
+	if ir {
+		img = l.IMG
+	}
+
+	// copy the img file
+	if !ir {
+		exist, err := FileExist("./static/img/front/")
+		if err != nil {
+			return err
+		}
+		if !exist {
+			err = os.Mkdir("./static/img/front/", os.ModeDir)
+			if err != nil {
+				return err
+			}
+		}
+
+		if img == "" {
+			return errors.New("the img file is required")
+		}
+		tails := strings.Split(img, "/")
+		if tails[len(tails)-1] == "" {
+			return errors.New("the given img file is a dir")
+		}
+		tails = strings.Split(tails[len(tails)-1], ".")
+		tail := tails[len(tails)-1]
+		err = Copy(img, "./static/img/front/"+fmt.Sprint(id)+"."+tail)
+		img = "./static/img/front/" + fmt.Sprint(id) + "." + tail
+	}
+
+	// insert the data to db
+	_, err = db.Exec(
+		`INSERT INTO CATEGORY (ID, Name, URL, Description,IMG ,FatherCategory, Priority, Tags)
+		VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+		id,
+		name,
+		url,
+		d,
+		img,
+		fcs,
+		priority,
+		ts,
+	)
+	if err != nil {
+		return err
+	}
+
+	// update the father category
+	var link []int64
+	var ls []byte
+	var idj int64
+	for i := 0; i < len(fcl); i += 1 {
+
+		idj = fcl[i]
+		err = db.QueryRow("SELECT Links FROM Category WHERE ID = ?", idj).Scan(&ls)
+		if errors.Is(err, sql.ErrNoRows) {
+
+		} else if err != nil {
+			return err
+		} else {
+
+			if len(ls) == 0 {
+				link = make([]int64, 0, 1)
+			} else {
+				err = json.Unmarshal(ls, &link)
+				if err != nil {
+					return err
+				}
+			}
+
+			link = append(link, int64(id))
+			ls, err = json.Marshal(link)
+			if err != nil {
+				return err
+			}
+
+			_, err = db.Exec("UPDATE Category SET Links = ? WHERE ID = ?", ls, idj)
+			if err != nil {
+				return err
+			}
+
+		}
+
+	}
+
 	return nil
 }
 
@@ -314,4 +523,42 @@ func AddLink(name string, url string, d string, img string, fc []string, priorit
 // id is used to store the img file, if the target file exist, this will replace it
 func FetchLinkInformation(url string, id int, imgRequired bool) (l *Link, err error) {
 	return nil, nil
+}
+
+// Copy the src file to dst. Any existing file will be overwritten and will not
+// copy file attributes.
+func Copy(src, dst string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return err
+	}
+
+	err = in.Close()
+	if err != nil {
+		return err
+	}
+
+	return out.Close()
+}
+
+// test if file exist
+func FileExist(path string) (exist bool, err error) {
+	_, err = os.Stat(path)
+	if errors.Is(err, os.ErrNotExist) {
+		return false, nil
+	}
+	if err != nil {
+		return false, err
+	}
+	return true, nil
 }
